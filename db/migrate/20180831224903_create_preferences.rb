147fc440e94be62ff830a94f0989de46a6dd81f2
Brennan Fulmer <brennan.flmr@gmail.com>
2018-09-01 01:31:39 +0000

    Done.

--- db/migrate/20180831224903_create_preferences.rb
+++ db/migrate/20180831224903_create_preferences.rb
@@ -1,8 +1,8 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
-      t.string :song_sort
-      t.string :artist_sort
+      t.string :song_sort_order
+      t.string :artist_sort_order
       
       t.timestamps null: false
     end
