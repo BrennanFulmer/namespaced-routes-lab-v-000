0f0627218ac0148202d770e557ff32455b1941af
Brennan Fulmer <brennan.flmr@gmail.com>
2018-09-01 01:23:03 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,7 +11,7 @@ class ArtistsController < ApplicationController
     if Preference.first.allow_create_artists
       @artist = Artist.new
     else
-      artists_path
+      redirect_to artists_path
     end
   end
 
